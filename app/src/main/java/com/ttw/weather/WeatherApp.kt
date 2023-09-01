5990b9da598134a0ba217d2d64bfc75bbccc728a
HtunHtunWin7 <htunhtunwin.ttw7@gmail.com>
2023-09-02 01:05:41 +0630

    api setup ok

---
+++ app/src/main/java/com/ttw/weather/WeatherApp.kt
@@ -0,0 +1,13 @@
+package com.ttw.weather
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
